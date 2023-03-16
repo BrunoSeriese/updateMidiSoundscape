6b1e126fbe7f7a89f119a5e99d9f0d0c2de90185
Bruno <bruno.pleysier@gmail.com>
2023-03-16 20:26:57 +0100

    all sounds back

--- main/main.ino
+++ main/main.ino
@@ -13,9 +13,9 @@ void setup() {
     Serial.begin(115200);
 
     scaper.start();
-    
+    scaper.addSound(soundPaths[0]);
     scaper.addSound(soundPaths[1]);
-   
+    scaper.addSound(soundPaths[2]);
     // scaper.clearSounds();
     scaper.changeSoundVolume(1, 0.8);
     scaper.changeSoundVolume(2, 2);
